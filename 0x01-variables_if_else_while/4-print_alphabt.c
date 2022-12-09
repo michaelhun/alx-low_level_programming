00140647738de3b8e555813b5819f39f2d0333a9
michaelhun <mikhun12@gmail.com>
2022-12-09 14:16:41 -0800

    four

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -6,14 +6,16 @@
  */
 int main(void)
 {
-	c = 'a';
-	while
-		(c <= 'z') {
-			if (c != 'e' && c != 'q')
-			{
-				putchar(c);
-			c++;
+	char c = 'a';
+
+	while (c <= 'z')
+	{
+		if (c != 'e' && c != 'q')
+		{
+			putchar(c);
 		}
+			c++;
+
 	putchar('\n');
 	return (0);
 }
