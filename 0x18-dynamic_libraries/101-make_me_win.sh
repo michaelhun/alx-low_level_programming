c344435b011326e9a29056762f6911ec0545e683
michaelhun <mikhun12@gmail.com>
2023-03-20 15:49:38 -0700

    last

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -p https://github.com/michaelhun/alx-low_level_programming/blob/master/0x18-dynamic_libraries/last_task.so
-export LD_PRELOAD="$PWD/../last_task.so"
+wget -P /tmp/ https://github.com/michaelhun/alx-low_level_programming/blob/master/0x18-dynamic_libraries/last_task.so
+export LD_PRELOAD="/tmp/last_task.so"
