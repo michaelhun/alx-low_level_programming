fc08ca2dbee99fa207b2d9c49d72c845877c2807
michaelhun <mikhun12@gmail.com>
2023-03-22 04:00:42 -0700

    last

--- 0x18-dynamic_libraries/last_task.c
+++ 0x18-dynamic_libraries/last_task.c
@@ -2,10 +2,16 @@
 #include<unistd.h>
 #include<string.h>
 #include<stdlib.h>
+/**
+ * printf - print the lottery
+ * @format:  display
+ * Return: return last task
+*/
 
 int printf(const char *format, ...)
 {
 	write(1, "9 8 10 24 75 -9\n", 17);
 	write(1, "Congragulation, you win the jackpot!\n", 38);
 	exit(EXIT_SUCCESS);
+	return (0);
 }
